8f2ef63dba5a039a987d0588b8b65e717300a535
Vince Name <rickcruz139@gmail.com>
2025-09-13 11:59:50 +0800

    first commit

--- app/controllers/UsersController.php
+++ app/controllers/UsersController.php
@@ -29,7 +29,7 @@ class UsersController extends Controller {
             );
 
             if ($this->usersModel->insert($data)) {
-    header('Location: /index.php/');
+    header('Location: /index/');
     exit;
 } else {
     echo "Error inserting record.";
@@ -56,7 +56,7 @@ class UsersController extends Controller {
             );
 
             if ($this->usersModel->update($id, $data)) {
-    header('Location: /index.php/');
+    header('Location: /index/');
     exit;
 } else {
                 echo "Error updating record.";
@@ -70,7 +70,7 @@ class UsersController extends Controller {
     public function delete($id)
     {
        if ($this->usersModel->delete($id)) {
-    header('Location: /index.php/');
+    header('Location: /index/');
     exit;} else {
             echo "Error deleting record.";
         }
