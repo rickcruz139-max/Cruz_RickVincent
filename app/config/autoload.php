49318600edeab6a7a29dde277be57c52e551f783
Vince Name <rickcruz139@gmail.com>
2025-09-27 17:43:23 +0800

    first commit

--- app/config/autoload.php
+++ app/config/autoload.php
@@ -82,7 +82,7 @@ $autoload['libraries'] = array('database', 'upload', 'session', 'pagination', 'f
 |
 |	$autoload['helpers'] = array('url', 'file');
 */
-$autoload['helpers'] = array('url');    
+$autoload['helpers'] = array('url');
 
 /*
 | -------------------------------------------------------------------
